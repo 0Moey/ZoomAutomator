fe7fc6a75aff28d3b300fcf8a82966ae6b43a2e0
thetottyapple <74386430+thetottyapple@users.noreply.github.com>
2021-02-11 20:54:10 -0600

    Rough Final Draft

---
+++ ZoomAutomator.py
@@ -0,0 +1,21 @@
+import keyboard, time, subprocess
+import pandas as pd
+from datetime import datetime, date
+import os
+import subprocess
+
+while True:
+  cached_day = date.today().weekday()
+  target_csv = pd.read_csv('map.csv').Type.values[cached_day]
+  current_csv = pd.read_csv(target_csv)
+  while date.today().weekday() == cached_day:
+    timestr = datetime.now().strftime("%H:%M")
+    if timestr in current_csv.Time.values:
+      index = current_csv.Time.values.tolist().index(timestr)
+      ID = current_csv.ID.values[index]
+      PASS = current_csv.Pass.values[index]
+      os.startfile(f'zoommtg://zoom.us/join?confno={ID}&pwd={PASS}&zc=0&browser=chrome')
+      time.sleep(60)
+    else:
+      time.sleep(1)
+  time.sleep(1)
